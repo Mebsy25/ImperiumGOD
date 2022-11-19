7a5ac51f82f038ebed45107c8a39115b1eb7cfca
Mebsy25 <sebastiangiron25@gmail.com>
2022-11-19 11:30:41 -0300

    roelsping

--- commands/informacion/mudae.js
+++ commands/informacion/mudae.js
@@ -5,11 +5,7 @@ module.exports = {
     name: 'mudae',
     alias: ['waifu'],
 run (client, message, args){
-    const embed1 = new EmbedBuilder()
-    .setTitle('🤖𝐵𝑜𝑡 𝐼𝑛𝑎𝑐𝑡𝑖𝑣𝑒🤖')
-    .setDescription(`**NO SIRVO**`)
-    .setColor(client.color)
-    .setImage(`https://cdn.discordapp.com/emojis/936312132079865886.gif?size=128&quality=lossless`)
-message.channel.send({embeds: [embed1]})
+    msg.channel.send(`<@&${1038591232483524619}> Found one!!`);
+
     }
 }
