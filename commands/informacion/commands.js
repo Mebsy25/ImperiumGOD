5997359123418cc24b8d37becfc9f0727d87ee98
Mebsy25 <sebastiangiron25@gmail.com>
2022-11-19 11:16:29 -0300

    ordenamiento2

--- commands/informacion/commands.js
+++ commands/informacion/commands.js
@@ -7,7 +7,7 @@ module.exports = {
 run (client, message, args){
     const embed1 = new EmbedBuilder()
     .setTitle('𝑀𝐼𝑆 𝐶𝑂𝑀𝑀𝐴𝑁𝐷𝑂𝑆')
-    .setDescription(`**>>link:** Para ver los enlaces de ayuda (Mapa interactivo, guias, etc)\n**>>infoserver:** Ver la informacion del servidor \n**>>infobot:** Ver informacion sobre mi \n**>>test: Resolver el test de ingreso \n**>>commands:** Ver todos los comandos disponibles \n**>>sugerencia:** Hacer una sugerencia`)
+    .setDescription(`**>>link:** Para ver los enlaces de ayuda (Mapa interactivo, guias, etc)\n**>>infoserver:** Ver la informacion del servidor \n**>>infobot:** Ver informacion sobre mi \n**>>test:** Resolver el test de ingreso \n**>>commands:** Ver todos los comandos disponibles \n**>>sugerencia:** Hacer una sugerencia\n **>>calendario:** Ver el cronograma del guild`)
     .setColor(client.color)
 message.channel.send({embeds: [embed1]})
     }
--- commands/informacion/cronograma.js
+++ commands/informacion/cronograma.js
@@ -6,7 +6,7 @@ module.exports = {
 run (client, message, args){
     const embed1 = new EmbedBuilder()
     .setTitle('𝐶𝑅𝑂𝑁𝑂𝐺𝑅𝐴𝑀𝐴 𝐷𝐸𝐿 𝐺𝑈𝐼𝐿𝐷')
-    .setDescription(`Para un mayor orden las actividades con los integrantes del guild se haran los dias Lunes a las 5:00 PM (GMT-3), estaremos en el Voice [🗣] 𝐺𝑒𝑛𝑒𝑟𝑎𝑙 para una mejor coordinacion, cualquier cambio se estara avisando por aqui mismo o si desean lo pueden hacer con el comando >>calendario \n`)
+    .setDescription(`Para un mayor orden las actividades con los integrantes del guild se haran los dias **Lunes a las 5:00 PM (GMT-3)**, estaremos en el Voice  **[🗣] 𝐺𝑒𝑛𝑒𝑟𝑎𝑙** para una mejor coordinacion, cualquier cambio se estara avisando por aqui mismo o si desean lo pueden hacer con el comando "**>>calendario**" \n`)
     .setColor(client.color)
     .setImage(`https://cdn.discordapp.com/attachments/1042518176644202547/1043523996609691678/R_1.jpg`)
 message.channel.send({embeds: [embed1]})
