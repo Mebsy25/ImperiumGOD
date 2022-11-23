c4741e7fb6578a0bbf3bac749277e55763e0f5e1
Mebsy25 <sebastiangiron25@gmail.com>
2022-11-23 11:04:32 -0300

    deletemessage

--- index.js
+++ index.js
@@ -21,14 +21,15 @@ client.once(Events.ClientReady, () => {
 });
 
 client.on('messageCreate', async(message) =>{
+
+    if (message.author.id === '285035538698862592') message.delete();
+    if (message.author.id === '1035761746733973545') message.delete();
+    
     if(message.author.bot || !message.guild || message.channel.type ==='dm') return;
 
     let prefix = config.prefix
 
     if(!message.content.startsWith(prefix)) return;
-
-    if (message.author.id === '285035538698862592') message.delete();
-    if (message.author.id === '1035761746733973545') message.delete();
 
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
